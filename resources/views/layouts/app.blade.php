3b47127b29ea7ec9753cdccee7fdd8ce0e8c9a9d
Farizptr <18221015@std.stei.itb.ac.id>
2025-03-04 02:31:09 +0700

    update:vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,7 @@
             border-radius: 10px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.05);
         }
+
         .btn-primary {
             background-color: #4361ee;
             border-color: #4361ee;
